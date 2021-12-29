3fb5174754fba3ac01d5682687f336cb07cf848a
yuri-raiter <yurinraiter@gmail.com>
2021-12-29 17:52:36 -0300

    reorganize pages folders

--- pages/404/index.tsx
+++ pages/404/index.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
-import styles from './404.module.scss'
+import styles from './styles.module.scss'
 
 export default function Custom404() {
   const router = useRouter()
